4b0c029a51ad5919663ecff149f61155eba8eff6
KrazeeKoder <tony.tulai@gmail.com>
2015-12-18 01:51:25 -0500

    Final Changes to FuzzBizzNezz assignment

--- FuzzBizzNezz/main.m
+++ FuzzBizzNezz/main.m
@@ -6,12 +6,34 @@
 //  Copyright © 2015 Anthony Tulai. All rights reserved.
 //
 
-#import <Foundation/Foundation.h>
+#include <stdio.h>
 
-int main(int argc, const char * argv[]) {
-    @autoreleasepool {
-        // insert code here...
-        NSLog(@"Hello, World!");
+
+
+int main()
+{
+    int fuzz;
+    int bizz;
+    int nezz;
+    
+    printf("Enter your value for Fuzz:\n");
+    scanf("%d", &fuzz);
+    printf("Enter your value for Bizz:\n");
+    scanf("%d", &bizz);
+    printf("Enter your value for Nezz:\n");
+    scanf("%d", &nezz);
+    
+    for (int i = 1; i <= nezz; i++)
+    {
+        if ((i % fuzz == 0) && (i % bizz == 0))
+            printf("FuzzBizz\n");
+        else if (i % fuzz == 0)
+            printf("Fuzz\n");
+        else if (i % bizz == 0)
+            printf("Bizz\n");
+        else
+            printf("%d\n",i);
     }
+    
     return 0;
 }
